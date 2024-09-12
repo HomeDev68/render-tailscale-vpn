41c1c2e7adb5af379bae7cccede873b088f16b27
HomeDev68 <samhomelabs68@proton.me>
2024-09-12 23:27:15 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,9 @@
 # Install Tailscale
 curl -fsSL https://tailscale.com/install.sh | sh
 
+# Start Tailscaled
+tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock --tun=userspace-networking --socks5-server=localhost:1055 --outbound-http-proxy-listen=localhost:1055 &
+
 # Start Tailscale
 sudo tailscale up --auth-key=${TAILSCALE_AUTHKEY} --hostname=${TAILSCALE_HOSTNAME} --advertise-exit-node ${TAILSCALE_ADDITIONAL_ARGS}
 
